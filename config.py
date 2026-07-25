72eba0d4de13fb8c42c1489942573ef56700db0a
valentinasokolovskaa808-prog <valentinasokolovskaa808@gmail.com>
2026-07-25 21:10:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,5 @@
 import os
 
-# Автоматичне очищення токена від будь-яких випадкових пробілів
-raw_token = os.getenv("BOT_TOKEN", "8891216844:AAEtznGW5kgTs1cnTbHisY0FlX9ojrMrqTc")
-BOT_TOKEN = raw_token.strip().strip('"').strip("'").replace(" ", "").replace("\n", "").replace("\r", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8891216844:AAEtznGW5kgTs1cnTbHisY0FlX9ojrMrqTc")
 
-# Telegram User ID адміністратора
 ADMIN_IDS = [5270272994]
