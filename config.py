34b8db237e327147464eaa829adb0f6bf5b492a8
valentinasokolovskaa808-prog <valentinasokolovskaa808@gmail.com>
2026-07-25 19:53:49 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
 BOT_TOKEN = "8891216844:AAEtznGW5kgTs1cnTbHisY0FlX9ojrMrqTc"
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
+
+# Telegram User ID адміністратора
+ADMIN_IDS = [5270272994]
