58f12b0f844cd6971882d90df361b19012b62dd6
valentinasokolovskaa808-prog <valentinasokolovskaa808@gmail.com>
2026-07-25 21:20:52 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 # Отримуємо токен та автоматично видаляємо всі випадкові пробіли, переноси рядків або таби
-RAW_TOKEN = os.getenv("BOT_TOKEN", "8005346082:AAFNAIrcqqv9VvKr51SizaQJV1xXySMGS-U")
+RAW_TOKEN = os.getenv("BOT_TOKEN", "8005346082:AAGx52ien9wTW2vcSSA_mLBEaX-cPDBffPM")
 BOT_TOKEN = RAW_TOKEN.strip()
 
 ADMIN_IDS = [5270272994]
